ce40b7a2dd5242780b0689cc5ae46aba62d5bdc8
aritra <aritrarick2002@gmail.com>
2023-10-31 21:38:38 +0530

    Initial Commit

---
+++ SwiftUITodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITodo
+//
+//  Created by Aritra on 31/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITodo/SwiftUITodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoApp.swift
+//  SwiftUITodo
+//
+//  Created by Aritra on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
