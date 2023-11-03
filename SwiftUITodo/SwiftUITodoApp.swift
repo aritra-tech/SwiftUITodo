601669668c2fc1ce9e0c6701df37307197751f58
aritra <aritrarick2002@gmail.com>
2023-11-04 03:01:37 +0530

    Created `ViewModel` to perform buissness logic

--- SwiftUITodo/SwiftUITodoApp.swift
+++ SwiftUITodo/SwiftUITodoApp.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUITodoApp: App {
+    
+    @StateObject var todoListViewModel : TodoListViewModel = TodoListViewModel()
+    
     var body: some Scene {
         WindowGroup {
             NavigationView{
                 TodoListView()
-            }
+            }.environmentObject(todoListViewModel)
         }
     }
 }
---
+++ SwiftUITodo/ViewModels/TodoListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  TodoListViewModel.swift
+//  SwiftUITodo
+//
+//  Created by Aritra on 04/11/23.
+//
+
+import Foundation
+
+class TodoListViewModel: ObservableObject {
+    
+    @Published var todos: [TodoModel] = []
+    
+    init() {
+        getTodo()
+    }
+    
+    func getTodo() {
+        let newTodo = [
+            TodoModel(title: "First title", isCompleted: true),
+            TodoModel(title: "First title", isCompleted: false),
+            TodoModel(title: "Third", isCompleted: false)
+        ]
+        todos.append(contentsOf: newTodo)
+    }
+    
+    
+    func deleteTodo(indexSet: IndexSet) {
+        todos.remove(atOffsets: indexSet)
+    }
+    
+    func moveTodo(from: IndexSet, to: Int) {
+        todos.move(fromOffsets: from, toOffset: to)
+    }
+    
+    func addTodo(title: String) {
+        let newTodo = TodoModel(title: title, isCompleted: false)
+        todos.append(newTodo)
+    }
+}
